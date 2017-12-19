caf14325b5810cda4876b5423c87fb858ac54e72
Eugene Ilagin <eugene.ilagin@gmail.com>
2017-12-19 17:39:29 +0300

    #609: Add restore from seed in intro page

--- src/js/routes.js
+++ src/js/routes.js
@@ -470,12 +470,12 @@
         })
         .state('initialRecovery', {
           url: '/initialRecovery',
-          templateUrl: 'views/initialRecovery.html',
+          templateUrl: 'views/recovery.html',
           walletShouldBeComplete: false,
           needProfile: false,
           views: {
             main: {
-              templateUrl: 'views/initialRecovery.html',
+              templateUrl: 'views/recovery.html',
             },
           },
         })
